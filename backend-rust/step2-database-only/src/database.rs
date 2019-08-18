c7b0472fea019b46cbde03bbeb5ac79ffc97c343
vms <michail.vms@gmail.com>
2019-08-18 18:24:10 +0200

    improve comments

--- backend-rust/step2-database-only/src/database.rs
+++ backend-rust/step2-database-only/src/database.rs
@@ -4,7 +4,7 @@ use crate::sqlite;
 pub fn query(query: &str) -> AppResult<String> {
     let response = sqlite::call(query.as_bytes());
 
-    // Decode query result to a utf8 string
+    // Add more details to error message
     String::from_utf8(response).map_err(|e| {
         log::error!("unable to decode result from bytes: {:#x?}", bytes);
         err_msg(&format!(
