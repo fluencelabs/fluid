74d57cd4088da69401882dcc67ec1b7b7eebf55c
vms <michail.vms@gmail.com>
2019-08-18 18:21:12 +0200

    rename bytes to query

--- backend-rust/step3-finished-app/src/database.rs
+++ backend-rust/step3-finished-app/src/database.rs
@@ -1,15 +1,15 @@
 use crate::errors::{err_msg, AppResult};
 use crate::sqlite;
 
-pub fn query(bytes: &str) -> AppResult<String> {
-    let response = sqlite::call(bytes.as_bytes());
+pub fn query(query: &str) -> AppResult<String> {
+    let response = sqlite::call(query.as_bytes());
 
     // Decode query result to a utf8 string
     String::from_utf8(response).map_err(|e| {
         log::error!("unable to decode result from bytes: {:#x?}", bytes);
         err_msg(&format!(
             "unable to decode result from bytes {:#x?}: {}",
-            bytes, e
+            query, e
         ))
     })
 }
