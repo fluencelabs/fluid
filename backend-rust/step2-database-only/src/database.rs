3b1d45a4d79ce680ade296280a326f212a41f952
vms <michail.vms@gmail.com>
2019-08-18 18:19:35 +0200

    more optimize

--- backend-rust/step2-database-only/src/database.rs
+++ backend-rust/step2-database-only/src/database.rs
@@ -5,15 +5,11 @@ pub fn query(bytes: &str) -> AppResult<String> {
     let response = sqlite::call(bytes.as_bytes());
 
     // Decode query result to a utf8 string
-    match String::from_utf8(response) {
-        Ok(string) => Ok(string),
-        Err(err) => {
-            log::error!("unable to decode result from bytes: {:#x?}", bytes);
-            Err(err_msg(&format!(
-                "unable to decode result from bytes {:#x?}: {}",
-                bytes, err
-            )))
-        }
-    }
-
+    String::from_utf8(response).map_err(|e| {
+        log::error!("unable to decode result from bytes: {:#x?}", bytes);
+        err_msg(&format!(
+            "unable to decode result from bytes {:#x?}: {}",
+            bytes, e
+        ))
+    })
 }
