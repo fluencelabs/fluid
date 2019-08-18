b2fdcd889e77449c62a8b09c39fbaa50bce66153
vms <michail.vms@gmail.com>
2019-08-18 18:16:22 +0200

    optimize sqlite call

--- backend-rust/step2-database-only/src/database.rs
+++ backend-rust/step2-database-only/src/database.rs
@@ -1,24 +1,19 @@
 use crate::errors::{err_msg, AppResult};
 use crate::sqlite;
 
-pub fn query(query: &str) -> AppResult<String> {
-    let response = sqlite::call(query.as_bytes());
+pub fn query(bytes: &str) -> AppResult<String> {
+    let response = sqlite::call(bytes.as_bytes());
 
     // Decode query result to a utf8 string
-    let result_str = std::str::from_utf8(&response);
-
-    // Log if there's an error
-    if result_str.is_err() {
-        log::error!("unable to decode result from bytes: {:#x?}", response);
+    match String::from_utf8(response) {
+        Ok(string) => Ok(string),
+        Err(err) => {
+            log::error!("unable to decode result from bytes: {:#x?}", bytes);
+            Err(err_msg(&format!(
+                "unable to decode result from bytes {:#x?}: {}",
+                bytes, err
+            )))
+        }
     }
 
-    // Wrap error with a better message, and return Result
-    result_str
-        .map_err(|e| {
-            err_msg(&format!(
-                "unable to decode result from bytes {:#x?}: {}",
-                response, e
-            ))
-        })
-        .map(|s| s.to_string())
 }
