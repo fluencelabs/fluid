416ecd359b712a0f3b99bc556f4b4bbb1c826933
Igor Krivchun <igor@krivchun.ru>
2019-08-20 14:35:57 +0700

    Fix messages fetching

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -5,12 +5,6 @@ import Message from "./Message";
 import {handleInitialData, fetchPosts} from "../actions/messages";
 
 class Dashboard extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {counter: 1, lastUpdateCounter: 0, firstFetch: true};
-    }
-
     componentDidMount() {
         this.props.dispatch(handleInitialData());
         this.pollingMessages = setInterval(
@@ -24,30 +18,15 @@ class Dashboard extends Component {
     }
 
     getMessages() {
-        this.props.dispatch(fetchPosts(this.state.counter + 1, this.state.firstFetch));
-
-        this.setState((state) => ({
-            counter: state.counter + 1,
-            firstFetch: false
-        }));
-    }
-
-    shouldComponentUpdate(nextProps) {
-        return nextProps.updates.counter > this.state.lastUpdateCounter
+        this.props.dispatch(fetchPosts());
     }
 
     render() {
-        let updates = this.props.updates;
-        if (updates.counter) {
-            this.setState(() => ({
-                lastUpdateCounter: updates.counter
-            }));
-        }
         return (
             <div>
                 <h3 className="center">Feed</h3>
                 <ul className="dashbord-list">
-                    {updates.messages.map((m, i) => {
+                    {this.props.updates.messages.map((m, i) => {
                         return (
                             <li key={i}>
                                 <Message message={m} />
