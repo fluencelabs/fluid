34815dc57811873d19ca8ad43441586934f7f41d
vms <michail.vms@gmail.com>
2019-08-20 13:46:02 +0200

    the first version of c backend

---
+++ backend-c/README.md
@@ -0,0 +1,16 @@
+# Fluid
+
+The twitter-like application for Fluence.
+
+# How to build
+
+This app could be built either with docker 
+
+```bash
+docker-compose up
+```
+
+or by Makefile with [wasi-sdk](https://github.com/CraneStation/wasi-sdk) installed
+```bash
+make
+```
---
+++ backend-c/src/model.c
@@ -0,0 +1,67 @@
+#include "../sdk/side_module_api.h"
+//#include <string.h>
+//#include <stdlib.h>
+
+__MODULE_IMPORT(sqlite)
+int snprintf ( char * s, int n, const char * format, ... );
+
+// TODO: introduce error codes and security checks
+
+void create_scheme() {
+    const char create_sql[] = "CREATE TABLE messages(message text, username text)";
+    sqlite_call(create_sql, sizeof(create_sql));
+}
+
+char *add_post(const char *message, int message_length, const char *username, int username_length) {
+    // at now wasm-ld has 1024 bytes for stack permission by default - that why dynamic allocation here
+    const int request_size = message_length + username_length + 50;
+    char *add_sql = (char *)malloc(request_size);
+
+    const int add_sql_length = snprintf(add_sql, request_size, "INSERT INTO messages VALUES(%s, %s)", message, username);
+    if(add_sql_length < 0) {
+        return 0;
+    }
+
+    return sqlite_call(add_sql, add_sql_length);
+}
+
+char *get_all_posts(int offset, int count) {
+    // at now wasm-ld has 1024 bytes for stack permission by default - that why dynamic allocation here
+    char *get_sql = (char *)malloc(256);
+
+    const int get_sql_length = snprintf(get_sql, 256,
+            "SELECT json_group_array(\n"
+            "            json_object('message', message, 'username', username)\n"
+            "        ) AS json_result FROM (\n"
+            "            SELECT * FROM messages LIMIT %d OFFSET %d\n"
+            "        )", count, offset);
+    if(get_sql_length < 0) {
+        return 0;
+    }
+
+    return sqlite_call(get_sql, get_sql_length);
+}
+
+char *get_posts_by_username(const char *username, int username_length, int offset, int count) {
+    // at now wasm-ld has 1024 bytes for stack permission by default - that why dynamic allocation here
+    const int request_size = username_length + 300;
+    char *get_sql = (char *)malloc(request_size);
+
+    const int add_sql_length = snprintf(get_sql, request_size,
+            "\"SELECT json_group_array(\n"
+            "            json_object('message', message, 'username', username)\n"
+            "        ) AS json_result FROM (\n"
+            "                SELECT * FROM messages where username = '%s' LIMIT %d OFFSET %d\n"
+            "        )",
+            username, count, offset);
+    if(add_sql_length < 0) {
+        return 0;
+    }
+
+    return sqlite_call(get_sql, add_sql_length);
+}
+
+char *get_posts_count() {
+    const char count_sql[] = "SELECT COUNT(*) from messages";
+    return sqlite_call(count_sql, sizeof(count_sql));
+}
